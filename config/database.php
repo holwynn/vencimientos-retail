6dd11f9e8fc18acc1e0b9816b7b73e9f1f0721e1
Ariel Holowinski <arielhr@protonmail.com>
2018-08-06 20:40:15 -0300

    use in-memory sqlite db for tests

--- config/database.php
+++ config/database.php
@@ -54,6 +54,12 @@ return [
             'engine' => null,
         ],
 
+        'testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prfix' => '',
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
