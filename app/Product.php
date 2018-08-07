5e42ae4694febd5195d613e7431048a0dc752b01
Ariel Holowinski <arielhr@protonmail.com>
2018-08-07 20:50:03 -0300

    allow searching products from walmart public db

--- app/Product.php
+++ app/Product.php
@@ -8,17 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    public const WALMART_URL = 'https://www.walmart.com.ar/api/catalog_system/pub/products/search?ft=';
+
     protected $fillable = [
         'name',
         'upc',
-        'qty',
         'img',
-        'expiration'
     ];
 
     protected $hidden = [
         'user_id'
     ];
+
+    public function fromWalmart($upc) {
+        $res = file_get_contents(self::WALMART_URL . $upc);
+        $json = json_decode($res);
+
+        if (!$json) {
+            return false;
+        }
+
+        $this->upc = $upc;
+        $this->name = $json[0]->productName;
+        $this->img = $json[0]->items[0]->images[0]->imageUrl;
+
+        return $this;
+    }
 
     public function user() {
         return $this->belongsTo(User::class);
