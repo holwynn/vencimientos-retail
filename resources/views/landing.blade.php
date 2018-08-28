734de24c53c15455d1e4534937a0295ce74c1d8a
Ariel Holowinski <ahw@tuta.io>
2018-08-28 01:58:06 -0300

    update placeholder landing;

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,32 +1,62 @@
-@extends('layouts.index')
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="favicon.png" type="image/png"/>
+  <link rel="shortcut icon" href="favicon.png" type="image/png"/>
 
-@section('content')
-<div class="container-fluid navbar-wrapper">
-  <div class="container">
-    <div class="row">
-      <ul class="nav">
-        <li class="nav-item">
-          <img height="50px;" src="{{ asset('assets/logo.png') }}" alt="" class="nav-link">
-        </li>
-        <li class="nav-item justify-content-end">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
+  <title>Vencify</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+  <!-- Styles -->
+  <style>
+  html, body {
+    background-color: #fff;
+    color: #636b6f;
+    font-family: 'Nunito', sans-serif;
+    font-weight: 200;
+    height: 100vh;
+    margin: 0;
+  }
+
+  .full-height {
+    height: 100vh;
+  }
+
+  .flex-center {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
+
+  .position-ref {
+    position: relative;
+  }
+
+  .content {
+    text-align: center;
+  }
+
+  .title {
+    font-size: 84px;
+  }
+
+  .m-b-md {
+    margin-bottom: 30px;
+  }
+</style>
+</head>
+<body>
+  <div class="flex-center position-ref full-height">
+    <div class="content">
+      <div class="title m-b-md">
+        Vencify
+      </div>
     </div>
   </div>
-</div>
-
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <h3>VenciPro</h3>
-    </div>
-  </div>
-</div>
-@endsection
+</body>
+</html>
